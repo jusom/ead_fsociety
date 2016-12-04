101e773373c3a972f6e9ba8f3c63df0f15ff30a7
Thauã Santos <nero.rbr@gmail.com>
2016-12-04 18:18:15 -0200

    Criação parcial do controlador principal

--- Classes/Controladores.php
+++ Classes/Controladores.php
@@ -4,8 +4,21 @@
  * Esta classe é responsável pelos controladores do padrão MVC no nosso sistema
  */
 
+/*
+ * TODO: inserir a função de verificação de login neste construtor
+ */
+
 namespace Classes;
 
 class Controladores {
-    //put your code here
+    
+    public $parametros = array();
+    
+    public function __construct( $parametros = array()) {
+    
+        $this->parametros = $parametros;
+    
+    }//construtor
+    
+    
 }
